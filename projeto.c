c48509bb830a3c88b46b6adafab2c73bb46f6433
kevenny03 <kevennykeke@gmail.com>
2019-05-24 14:47:18 -0300

    .

--- projeto.c
+++ projeto.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
 int escolha_home;
+
 int home_page(){
 printf("1-Cadastrar Usuario\n");
 printf("2-Cadastrar livro\n");
@@ -9,12 +9,12 @@ printf("4-Devolucao\n");
 printf("5-Reserva\n\n");
 printf("Escolha: ");
 scanf("%d",&escolha_home);
-acoes();
 }
 
-void acoes(){
-    if(escolha_home==1){
+intacoes(){
 
+    if(escolha_home==1){
+        
     }
     else if(escolha_home==2){
 

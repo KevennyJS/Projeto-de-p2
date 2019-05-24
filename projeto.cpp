5f1a0cd7cb555dc8d571bfd29126e06d055f42b8
kevenny03 <kevennykeke@gmail.com>
2019-05-24 16:45:25 -0300

    .

--- projeto.cpp
+++ projeto.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-int escolha_home;
+char escolha_home;
 
-struct Cadastro_usuario{
+struct cadastro_usuario{
 
 char nome[25];
 int cpf;
@@ -12,6 +12,26 @@ char endereco[30];
 
 };
 
+int cadastro(){
+    int num_user;
+    struct cadastro_usuario reg[num_user];
+        int nome,cpf,telefone,endereco;
+        cout<<"Nome: ";
+        cin>>reg[num_user].nome;
+        fflush(stdin);
+        cout<<"CPF: ";
+        cin>>reg[num_user].cpf;
+        fflush(stdin);
+        cout<<"Telefone: ";
+        cin>>reg[num_user].telefone;
+        fflush(stdin);
+        cout<<"Endereço: ";
+        cin>>reg[num_user].endereco;
+        fflush(stdin);
+        num_user +1;
+}
+
+
 int home_page(){
 cout<<"1-Cadastrar Usuario\n";
 cout<<"2-Cadastrar livro\n";
@@ -19,13 +39,12 @@ cout<<"3-Emprestimo\n";
 cout<<"4-Devolucao\n";
 cout<<"5-Reserva\n\n";
 cout<<"Escolha: ";
-scanf("%d",&escolha_home);
-}
+cin>>escolha_home;
 
-int acoes(){
     switch (escolha_home){
-        case'1':
 
+        case'1':
+        cadastro();
 
         case'2':
 
@@ -41,7 +60,7 @@ int acoes(){
 
         default:
             system("CLS");
-            cout<< "Escolha uma opcao valida!\n";
+            cout<< "Escolha uma opcao valida!\n\n";
             home_page();
         }
 }
